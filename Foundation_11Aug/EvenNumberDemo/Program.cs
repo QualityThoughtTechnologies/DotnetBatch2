44b99acf52deb1ec746ecd378d06ed723f8103a6
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-08-11 07:59:39 +0530

    Basic Even Number

---
+++ Foundation_11Aug/EvenNumberDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumberDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 76;
+            if(number%2==0)
+            {
+                Console.WriteLine("Even Number");
+            }
+
+            //note: The else is commented to check different if statement
+            //else
+            if(number%2!=0)
+            {
+                Console.WriteLine("Odd Number");
+            }
+        }
+    }
+}
