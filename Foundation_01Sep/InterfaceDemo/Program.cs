aeac06704afce94f700d9362a91ae5bfae89a315
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-01 10:20:36 +0530

    InterfaceDemo

---
+++ Foundation_01Sep/InterfaceDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using ImplementationLibrary.Bank;
+using InterfaceLibrary.Bank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBankAccount savingsAccount = new SavingsAccount();
+            IBankAccount currentAccount = new CurrentAccount();
+
+            savingsAccount.Deposit(1000);
+            DisplayBalance(savingsAccount);
+            savingsAccount.Withdraw(100);
+            DisplayBalance(savingsAccount);
+            savingsAccount.Withdraw(2000);
+            DisplayBalance(savingsAccount);
+
+            currentAccount.Deposit(10000);
+            DisplayBalance(currentAccount);
+            currentAccount.Withdraw(15000);
+            DisplayBalance(currentAccount);
+
+
+
+        }
+
+        private static void DisplayBalance(IBankAccount account)
+        {
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
