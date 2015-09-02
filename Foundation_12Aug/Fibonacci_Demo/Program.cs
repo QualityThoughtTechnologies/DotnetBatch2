d3238e9e9caf002278b61073597db78f0e0b884d
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-02 19:45:07 +0530

    Commit Changes to make build fixes

--- Foundation_12Aug/Fibonacci_Demo/Program.cs
+++ Foundation_12Aug/Fibonacci_Demo/Program.cs
@@ -17,8 +17,10 @@ namespace Fibonacci_Demo
             string input = Console.ReadLine();
             Console.Write("1 2 ");
             int number = int.Parse(input);
-
-            while (fibonnaci<number)
+            int sum = 2;
+            int index = 1;
+            while(index<=50)
+            //while (fibonnaci<number)
             {
                 fibonnaci = firstNumber + secondNumber;
                 if(fibonnaci>number)
@@ -29,6 +31,11 @@ namespace Fibonacci_Demo
                 Console.Write(" ");
                 firstNumber = secondNumber;
                 secondNumber = fibonnaci;
+                if(fibonnaci%2==0)
+                {
+                    sum += fibonnaci;
+                    index++;
+                }
             }
 
             Console.ReadLine();
---
+++ Foundation_12Aug/PalindromeDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int number = 100;number<=999;number++)
+            {
+                int tempNumber = number;
+                int reverse = 0;
+                while (tempNumber != 0)
+                {
+                    int remainder = tempNumber % 10;
+                    tempNumber = tempNumber / 10;
+                    reverse = reverse * 10 + remainder;
+
+                }
+
+                if (number == reverse)
+                {
+                    Console.Write(number + " ");
+                }
+            }
+
+            
+        }
+    }
+}
