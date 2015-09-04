a2908689455b946a34517d910f38d2debdd51b78
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-04 08:23:05 +0530

    Structure & enum

---
+++ Foundation_04Sep/StructureDemo/BookINfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureDemo
+{
+    struct BookInfo
+    {
+        public string Id;
+
+        public string Name;
+
+        public string Author;
+
+        public Category Category;
+
+
+
+    }
+}
---
+++ Foundation_04Sep/StructureDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookInfo bookInfo=new BookInfo();
+            bookInfo.Author = "Test";
+            bookInfo.Id = "1";
+            bookInfo.Name = "Test";
+            Category category=(Category) Enum.Parse(typeof(Category), "Mathematics");
+            bookInfo.Category = Category.Mathematics;
+        }
+    }
+}
