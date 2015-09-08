2c9d9c1824283e40c38b399955a77a3f6706b706
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-08 07:31:45 +0530

    Lists

---
+++ Foundatinon_07Sep/ParkingStationRevisitedDemo/ParkingStation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingStationRevisitedDemo
+{
+    class ParkingStation
+    {
+        IList<Bike> bikes = new List<Bike>();
+
+        public void Park(Bike bike)
+        {
+            bikes.Add(bike);
+        }
+
+        public void UnPark(int number)
+        {
+            Bike bike;
+            int foundAt = -1;
+            for(int index=0;index<bikes.Count;index++)
+            {
+                if(bikes[index].Number==number)
+                {
+                    //bike = bikes[index];
+                    foundAt = index;
+                    break;
+                }
+            }
+            //bikes.Remove(bike);
+            bikes.RemoveAt(foundAt);
+            //foreach(Bike bikeI in bikes)
+            //{
+            //    if(bikeI.Number==number)
+            //    {
+            //        bike = bikeI;
+            //        break;
+            //    }
+            //}
+            ////bikes.Remove(bike);
+
+            //bikes.Remove(bikes.FirstOrDefault(item => item.Number == number));
+            //from bike in bikes where 
+            
+        }
+
+        public void ShowAll()
+        {
+            foreach (Bike bike in bikes)
+            {
+                Console.WriteLine(bike.Number);
+            }
+        }
+    }
+}
---
+++ Foundatinon_07Sep/ParkingStationRevisitedDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingStationRevisitedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingStation p = new ParkingStation();
+            p.Park(new Bike() { Name = "Test", Number = 1 });
+            p.Park(new Bike() { Name = "Test", Number = 2 });
+            p.Park(new Bike() { Name = "Test", Number = 3 });
+            p.Park(new Bike() { Name = "Test", Number = 4 });
+            p.Park(new Bike() { Name = "Test", Number = 5 });
+            p.Park(new Bike() { Name = "Test", Number = 6 });
+            p.Park(new Bike() { Name = "Test", Number = 7 });
+            p.ShowAll();
+            p.UnPark(5);
+            p.UnPark(6);
+            p.ShowAll();
+        }
+    }
+}
