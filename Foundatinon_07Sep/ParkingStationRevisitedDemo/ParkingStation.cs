fa6e60531bd981d210de8e500a4e5dbeb65bf780
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-08 08:28:41 +0530

    INdexer

--- Foundatinon_07Sep/ParkingStationRevisitedDemo/ParkingStation.cs
+++ Foundatinon_07Sep/ParkingStationRevisitedDemo/ParkingStation.cs
@@ -9,10 +9,13 @@ namespace ParkingStationRevisitedDemo
     class ParkingStation
     {
         IList<Bike> bikes = new List<Bike>();
+        Dictionary<int, Bike> parkingDictionary = new Dictionary<int, Bike>();
 
         public void Park(Bike bike)
         {
             bikes.Add(bike);
+            parkingDictionary.Add(bike.Number, bike);
+            parkingDictionary[bike.Number] = bike;
         }
 
         public void UnPark(int number)
@@ -45,12 +48,39 @@ namespace ParkingStationRevisitedDemo
             
         }
 
+        public string this[int value]
+        {
+            get
+            {
+                return parkingDictionary[value].Name;
+            }
+        }
+
+        public void EffecientUnPark(int number)
+        {
+            if(parkingDictionary.ContainsKey(number))
+            {
+                parkingDictionary.Remove(number);
+            }
+        }
+
         public void ShowAll()
         {
             foreach (Bike bike in bikes)
             {
                 Console.WriteLine(bike.Number);
             }
+        }
+
+        public void ShowKeysAndValues()
+        {
+            //foreach (KeyValuePair<int,Bike> item in parkingDictionary)
+            //{
+            //    Console.WriteLine(item.Key);
+                
+            //}
+            Console.WriteLine(string.Join(Environment.NewLine, parkingDictionary.Keys)); 
+
         }
     }
 }
--- Foundatinon_07Sep/ParkingStationRevisitedDemo/Program.cs
+++ Foundatinon_07Sep/ParkingStationRevisitedDemo/Program.cs
@@ -13,15 +13,21 @@ namespace ParkingStationRevisitedDemo
             ParkingStation p = new ParkingStation();
             p.Park(new Bike() { Name = "Test", Number = 1 });
             p.Park(new Bike() { Name = "Test", Number = 2 });
-            p.Park(new Bike() { Name = "Test", Number = 3 });
+            p.Park(new Bike() { Name = "TestMe", Number = 3 });
             p.Park(new Bike() { Name = "Test", Number = 4 });
             p.Park(new Bike() { Name = "Test", Number = 5 });
             p.Park(new Bike() { Name = "Test", Number = 6 });
             p.Park(new Bike() { Name = "Test", Number = 7 });
-            p.ShowAll();
+            //p.ShowAll();
+            p.ShowKeysAndValues();
             p.UnPark(5);
+            p.EffecientUnPark(5);
             p.UnPark(6);
-            p.ShowAll();
+            p.EffecientUnPark(6);
+            p.ShowKeysAndValues();
+
+            Console.WriteLine(p[3]);
+            //p.ShowAll();
         }
     }
 }
