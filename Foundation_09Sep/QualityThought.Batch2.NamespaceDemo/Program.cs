bf09e8ada75172e9092e1d73d1c9b1dfe7a07c59
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-09 08:06:37 +0530

    Namespace Demo

---
+++ Foundation_09Sep/QualityThought.Batch2.NamespaceDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using QualityThought.Batch2.NamespaceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QualityThought.Batch2.NamespaceLibrary.UI;
+using MyUI = QualityThought.Batch2.NamespaceLibrary.UI;
+using QualityThought.Batch2.NamespaceLibrary.Common;
+using MyCommon = QualityThought.Batch2.NamespaceLibrary.Common;
+
+namespace QualityThought.Batch2.NamespaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            QualityThought.Batch2.NamespaceLibrary.UI.TestUI testui = new NamespaceLibrary.UI.TestUI();
+            TestUI ui = new TestUI();
+            QualityThought.Batch2.NamespaceLibrary.Common.TestCommon testCommon = new NamespaceLibrary.Common.TestCommon();
+            TestCommon testC = new TestCommon();
+            MyCommon.TestMe testMe = new MyCommon.TestMe();
+            MyUI.TestMe testMeUI = new MyUI.TestMe();
+        }
+    }
+}
