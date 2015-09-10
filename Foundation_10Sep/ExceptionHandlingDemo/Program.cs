f574e662c8eee2f2958da6e476e319a4924a416a
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-10 08:32:44 +0530

    Exceptions

---
+++ Foundation_10Sep/ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <exception cref="System.ArgumentNullException"/>
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Enter any number");
+            try
+            {
+                int test = int.Parse(Console.ReadLine());
+                Console.WriteLine(test);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error Occured. Please Enter a valid value");
+                Console.WriteLine(" Enter any value to read error details, press Enter to exit");
+
+                string choice = Console.ReadLine();
+                if(!string.IsNullOrWhiteSpace(choice))
+                {
+                    Console.WriteLine("Message is "+ex.Message);
+                    Console.WriteLine("Stack Trace is "+ex.StackTrace);
+                }
+            }
+            //catch(Exception)
+            //{
+            //    Console.WriteLine("Enter valid value in the range of {0} an {1}", int.MinValue, int.MaxValue);
+            //}
+
+               // Console.WriteLine("Please visit us again");
+            finally
+            {
+                Console.WriteLine("Please visit us again");
+            }
+            
+
+        }
+    }
+}
