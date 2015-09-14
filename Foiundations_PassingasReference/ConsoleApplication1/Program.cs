be2fad09761b0c4c9a4463d7fcffece3d04a0876
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-14 08:24:48 +0530

    Ref & Out

---
+++ Foiundations_PassingasReference/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rate;
+            double time=5;
+            Console.WriteLine(GetInterest(10000, out rate, ref time)); ;
+            Console.WriteLine(rate);
+            Console.WriteLine(time);
+            //int.TryParse
+        }
+
+        static double GetInterest(double principle,out double rateOfIntrest,ref double timeInYears)
+        {
+            rateOfIntrest = 4;
+            timeInYears++;
+            return principle * timeInYears * rateOfIntrest / 100;
+        }
+    }
+}
