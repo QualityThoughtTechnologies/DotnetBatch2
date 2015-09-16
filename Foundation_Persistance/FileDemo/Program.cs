0086786446bb6649b884e66fd9c10c13092a7cc4
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-16 07:57:46 +0530

    File Demo

--- Foundation_Persistance/FileDemo/Program.cs
+++ Foundation_Persistance/FileDemo/Program.cs
@@ -11,8 +11,9 @@ namespace FileDemo
     {
         static void Main(string[] args)
         {
-            string userInput="Not";
-            do
+            Console.WriteLine("Do you want to add a new student record if No press enter");
+            string userInput=Console.ReadLine();
+            while (!string.IsNullOrWhiteSpace(userInput)) 
             {
                 Student s = new Student();
                 Console.WriteLine("Enter id");
@@ -33,21 +34,63 @@ namespace FileDemo
                 Console.WriteLine("Do you want to continue. Press Enter to exit");
                 userInput = Console.ReadLine();
                 File.AppendAllText("Students.txt", s.ToString()+Environment.NewLine);
-            } while (!string.IsNullOrWhiteSpace(userInput));
+            } 
 
             do{
                 string userChoice = "0";
                 Console.WriteLine("Enter 0 to display all the contents & 1 to search");
                 userChoice = Console.ReadLine();
+                IConvertor<Student, String> convertor = new StudentToStringConvertor();
+                IList<Student> studentList = new List<Student>();
+                AddStudentsToList(convertor, studentList);
                 if(userChoice.Equals("0"))
                 {
-                    Console.WriteLine(File.ReadAllText("Students.txt"));
+                    foreach(Student s in studentList)
+                    {
+                        Console.WriteLine("Name is {0}, Id is {1}, phone number is {2}",
+                            s.Name, s.Id, s.PhoneNumber);
+                    }
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Enter Id");
+                    int id = int.Parse(Console.ReadLine());
+                    bool isFound = false;
+                    foreach(Student s in studentList)
+                    {
+                        if(s.Id==id)
+                        {
+                            isFound = true;
+                            Console.WriteLine("Name is {0}, Id is {1}, phone number is {2}",
+                            s.Name, s.Id, s.PhoneNumber);
+                            break;
+                        }
+                    }
+                    if(!isFound)
+                    {
+                        Console.WriteLine("Could not find any record");
+                    }
                 }
 
                 Console.WriteLine("Do you want to continue. Press Enter to exit");
                 userInput = Console.ReadLine();
             } while (!string.IsNullOrWhiteSpace(userInput));
             
+        }
+
+        private static void AddStudentsToList(IConvertor<Student, String> convertor, IList<Student> studentList)
+        {
+            foreach (string line in File.ReadAllLines("Students.txt"))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+                Student student = convertor.ConvertFrom(line);
+                studentList.Add(student);
+
+            }
         }
     }
 }
