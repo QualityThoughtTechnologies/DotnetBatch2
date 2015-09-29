b1543b0063939c2959ff0b48d5b69ca317b253f7
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-09-29 07:55:19 +0530

    Stream Basics

---
+++ Foundation_StreamBasics/StreamBasics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamReader reader=new StreamReader("Read.txt"))
+            {
+                string line;
+                do
+                {
+                    line = reader.ReadLine();
+                    Console.WriteLine(line);
+                } while (!string.IsNullOrWhiteSpace(line));
+                }
+        }
+    }
+}
