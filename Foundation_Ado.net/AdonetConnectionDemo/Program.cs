e6a02e28eb4acd25838d5842334c1bd2b61f2f54
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-10-04 16:24:19 +0530

    ado.net

---
+++ Foundation_Ado.net/AdonetConnectionDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdonetConnectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=DotnetBatch2;Integrated Security=True";
+            //using(SqlConnection connection=new SqlConnection(connectionString))
+            //{
+            //    SqlCommand updateCommand = new SqlCommand("update student_master set ph_no='99999999' where ph_no IS NULL", connection);
+            //    connection.Open();
+            //    int rowsAffected=updateCommand.ExecuteNonQuery();
+            //    Console.WriteLine("Rows effected are {0}",rowsAffected);
+            //    connection.Close();
+            //}
+
+            //using(SqlConnection connection=new SqlConnection(connectionString))
+            //{
+            //    Console.WriteLine("Enter Student Id");
+            //    string id = Console.ReadLine();
+            //    Console.WriteLine("Enter Student Name");
+            //    string name = Console.ReadLine();
+            //    Console.WriteLine("Enter address");
+            //    string address = Console.ReadLine();
+            //    Console.WriteLine("Enter phone number");
+            //    string phoneNo = Console.ReadLine();
+            //    string insertCommand = "insert into student_master values('{0}','{1}','{2}','{3}')";
+            //    SqlCommand insertSqlCommand = new SqlCommand(
+            //        string.Format(insertCommand, id, name, address, phoneNo), connection);
+            //    connection.Open();
+            //    insertSqlCommand.ExecuteNonQuery();
+            //    connection.Close();
+
+            //}
+
+            using(SqlConnection connection=new SqlConnection(connectionString))
+            {
+                SqlCommand selectCommand = new SqlCommand("Select * from student_master", connection);
+                connection.Open();
+                using (SqlDataReader dataReader = selectCommand.ExecuteReader())
+                {
+                    while(dataReader.Read())
+                    {
+                        Console.WriteLine(dataReader["ph_no"]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Foundation_Ado.net/DataSetDemo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            using(SqlConnection connection=new SqlConnection("Data Source=.;Initial Catalog=DotnetBatch2;Integrated Security=True"))
+            {
+                SqlDataAdapter dataAdapter=new SqlDataAdapter("Select * from student_master",connection);
+                DataTable table=new DataTable();
+                dataAdapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            
+        }
+    }
+}
