e9d05bb7c8723c6328a651f7dd2d984c1b222513
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-10-05 08:58:23 +0530

    Delegates Demo

---
+++ Foundation_Delegates/DelegateDemo/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    class Message
+    {
+
+        
+        MessageConsole messageConsole;
+        public Message()
+        {
+             messageConsole = new MessageConsole(LogMessage);
+             messageConsole += Console.WriteLine;
+        }
+        public void Log(string mesage)
+        {
+            messageConsole(mesage);
+
+        }
+
+        public delegate void MessageConsole(string s);
+        void LogMessage(string message)
+        {
+            Console.WriteLine("{0}---->{1}", DateTime.Now, message);
+        }
+    }
+}
---
+++ Foundation_Delegates/DelegateDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Message message = new Message();
+            message.Log("Hi");
+            message.Log("How");
+            message.Log("are");
+            message.Log("you");
+        }
+
+       
+    }
+}
