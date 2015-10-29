37265edeb7abe8a6e89b2d43c973ba4f7a0bf878
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-10-29 15:11:28 +0530

    webservices  using  inprogram example

---
+++ TestWebService/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ip2GeoTestClient.IP2Geo geo = new Ip2GeoTestClient.IP2Geo();
+            Ip2GeoTestClient.IPInformation info = geo.ResolveIP("49.204.64.246", "0");
+            Console.WriteLine(info.Country);
+            CalculatorService.Calculator cal = new CalculatorService.Calculator();
+            Console.WriteLine(cal.add(3,5));
+        }
+    }
+}
